3ab9035e8808217f8bf18315b940770224d97916
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-02 09:16:54 +0330

    🌿feat: Change for saving Token

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,12 @@
-// lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true, // ← مهم برای ذخیره token
+      autoRefreshToken: true
+    }
+  }
+);
