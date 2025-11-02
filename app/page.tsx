41f2d0caaeb613613bb5e4cdc157418d7bae8874
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-02 09:17:33 +0330

    🌿feat: Check login and show the email

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,26 @@ import { supabase } from "@/lib/supabaseClient";
 
 export default function Dashboard() {
   const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUser = async () => {
-      const { data } = await supabase.auth.getSession();
-      console.log("data", data)
+    const {data} = await supabase.auth.getSession();
       setUser(data?.session?.user ?? null);
-    };
-    getUser();
+      setLoading(false);
+  };
+
+  getUser();
+
+  const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+    setUser(session?.user ?? null);
+    setLoading(false);
+  });
+
+    return () => listener.subscription.unsubscribe();
   }, []);
+
+  if (loading) return <div>Loading...</div>;
 
   if (!user) return <div>Please log in</div>;
 
