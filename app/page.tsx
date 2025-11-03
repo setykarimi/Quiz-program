2facd6206fd876c740e9d28ddfe10e21e18cb6a9
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-03 14:51:25 +0330

    🌿feat: Remove main page from / url

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
-
 export default function Dashboard() {
-  const [user, setUser] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
-  const [exams, setExams] = useState<any[]>([]);
-
-  useEffect(() => {
-    const getUser = async () => {
-    const {data} = await supabase.auth.getSession();
-      setUser(data?.session?.user ?? null);
-      setLoading(false);
-  };
-
-  getUser();
-
-  const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-    setUser(session?.user ?? null);
-    setLoading(false);
-  });
-
-    return () => listener.subscription.unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    const fetchExams = async () => {
-      setLoading(true);
-      const { data, error } = await supabase.from("exams").select("*");
-
-      if (error) {
-        console.error(error);
-      } else {
-        setExams(data || []);
-      }
-      setLoading(false);
-    };
-
-    fetchExams();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-
-  if (!user) return <div>Please log in</div>;
-
-
-  return <div>Welcome, {user?.user_metadata?.display_name}</div>;
+  return <div></div>;
 }
--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,4 @@
 export * from './form/input'
+
+
+export * from './sidebar'
