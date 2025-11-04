ddefc09e36c6d4e946be3b9be2a07935cc62a3de
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-04 12:10:12 +0330

    🌿feat: Show the name here

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { Sidebar } from "@/components";
-import { AuthProvider } from "@/context/auth-context";
+import { useAuth } from "@/context/auth-context";
 import type { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const { user } = useAuth();
+  
   return (
-    <AuthProvider>
-      <div className="bg-gray-100 min-h-screen p-4">
-        <div className="mx-auto max-w-350 grid gap-5 grid-cols-5">
-          <Sidebar />
-          <div className="p-3 rounded-lg bg-white col-span-4 h-fit">
-            {/* <div>Welcome, {user?.user_metadata?.display_name}</div> */}
+    <div className="bg-gray-100 min-h-screen p-4">
+      <div className="mx-auto max-w-350 grid gap-4 grid-cols-5">
+        <Sidebar />
+        <div className="col-span-4 h-fit">
+          <div className="p-4 rounded-xl bg-white text-sm mb-4">Welcome, <span className="font-bold">{user?.user_metadata?.display_name}</span></div>
+          <div className="bg-white p-4 rounded-xl">
             {children}
           </div>
         </div>
       </div>
-    </AuthProvider>
+    </div>
   );
 }
