4c4c2f5f519cb04bbe63598f860e21975e3a59df
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-04 12:08:57 +0330

    🌿feat: Check the user in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 "use client";
 
+import { useAuth } from "@/context/auth-context";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Dashboard() {
-  return <div></div>;
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.replace("/dashboard");
+    else if (!user) router.replace("/login");
+  }, []);
+
+  return null
 }
