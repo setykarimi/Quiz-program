6f16b0be83a2aa3385739d8bbfbdca981c884b15
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-08 12:20:39 +0330

    🌿feat: New context for setting

---
+++ context/setting-context.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { createContext, Dispatch, SetStateAction, useContext, useState } from "react";
+
+type SettingContextType = {
+    showSidebar: boolean
+    handleShowSidebar: () => void
+};
+
+const SettingContext = createContext<SettingContextType>({
+    showSidebar: false,
+    handleShowSidebar: () => {}
+});
+
+export const SettingProvider = ({ children }: { children: React.ReactNode }) => {
+    const [showSidebar, setShowSidebar] = useState(false)
+
+    const handleShowSidebar = () => {
+        setShowSidebar(!showSidebar)
+    }
+
+    return (
+        <SettingContext.Provider value={{ showSidebar, handleShowSidebar }}>
+            {children}
+        </SettingContext.Provider>
+    );
+};
+
+export const useSetting = () => useContext(SettingContext);
