7becd2ee755fa15a69141d17bcaa13a182a21039
ISQI\se.karimi <setykarimi80@gmail.com>
2025-11-26 14:30:04 +0330

    🌿feat: New page for user exams

---
+++ app/dashboard/member/user-exams/[id]/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { supabase } from "@/lib/supabaseClient";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+
+    const params = useParams();
+    const id = params.id;
+
+    console.log("id", id)
+
+    const { data: questions, isLoading: loadingQuestions } = useQuery({
+        queryKey: ["questions"],
+        queryFn: async () => {
+        const { data, error } = await supabase.from("questions").select("*").order("id");
+        if (error) throw error;
+        return data;
+        },
+        enabled: !!id,
+    });
+
+    const handleYes = () => {
+        console.log("Exam started");
+        // router.push("/exam/123")
+    };
+
+    const handleNo = () => {
+        console.log("Canceled");
+    };
+
+  return (
+    <div className="max-w-sm mx-auto p-6 ">
+      <h2 className="text-lg font-bold mb-2 text-gray-800">
+        Are you sure you want to start the exam?
+      </h2>
+
+      <p className="text-sm text-gray-500 mb-4">
+        After starting the exam, the timer and rules may activate.
+      </p>
+
+      <div className="h-px bg-gray-200 my-4" />
+
+      <div className="flex gap-3">
+        <button
+          onClick={handleYes}
+          className="flex-1 py-2.5 rounded-lg bg-green-600 text-white text-sm font-medium hover:bg-green-700 transition"
+        >
+          Yes
+        </button>
+
+        <button
+          onClick={handleNo}
+          className="flex-1 py-2.5 rounded-lg border border-gray-300 bg-gray-50 text-gray-700 text-sm font-medium hover:bg-gray-100 transition"
+        >
+          No
+        </button>
+      </div>
+    </div>
+  );
+}
